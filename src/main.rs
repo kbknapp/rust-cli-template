d56137692c55f7099f60bebe5dfe6b3ca8d50c4d
Kevin K <kbknapp@gmail.com>
2021-07-01 06:49:16 -0400

    fixes imports and template vars

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use clap::*;
+use std::env;
+
+use clap::Clap;
 
 #[macro_use]
 mod macros;
@@ -10,12 +12,12 @@ fn main() {
 
     match args.verbose {
         0 => match args.quiet {
-            0 => env::set_var("RUST_LOG", "{{project-name}}=info"),
-            1 => env::set_var("RUST_LOG", "{{project-name}}=warn"),
-            2 => env::set_var("RUST_LOG", "{{project-name}}=error"),
-            _ => env::set_var("RUST_LOG", "{{project-name}}=off"),
+            0 => env::set_var("RUST_LOG", "{{crate_name}}=info"),
+            1 => env::set_var("RUST_LOG", "{{crate_name}}=warn"),
+            2 => env::set_var("RUST_LOG", "{{crate_name}}=error"),
+            _ => env::set_var("RUST_LOG", "{{crate_name}}=off"),
         },
-        1 => env::set_var("RUST_LOG", "{{project-name}}=debug"),
-        _ => env::set_var("RUST_LOG", "{{project-name}}=trace"),
+        1 => env::set_var("RUST_LOG", "{{crate_name}}=debug"),
+        _ => env::set_var("RUST_LOG", "{{crate_name}}=trace"),
     }
 }
